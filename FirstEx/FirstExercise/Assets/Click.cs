fa508358b36c342bf5e136f4a2f19029c08757cd
crkteam <39040121+crkteam@users.noreply.github.com>
2018-05-08 00:19:38 +0800

    crkjk
    12:19還很多沒弄

---
+++ FirstEx/FirstExercise/Assets/Click.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Click : MonoBehaviour {
+
+    int cash;
+    public int num;
+    public Text t;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    public void save() {
+        LoadJS lj = new LoadJS();
+        lj.Load();
+
+        this.cash = lj.load.cash;
+        this.cash += num;
+
+        lj.Save(this.cash);
+    }
+}
